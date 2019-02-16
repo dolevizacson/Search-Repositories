ff71f85068f95c2fdc82e7e488765d48717d7555
Unknown <dolevizacson@gmail.com>
2019-02-16 23:42:11 +0200

    first commit
    add controller http request login, add partial view to render repositories, add model for repositories data, add ajax form to post search term to the controller

--- Search-Repositories/Controllers/HomeController.cs
+++ Search-Repositories/Controllers/HomeController.cs
@@ -3,28 +3,73 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using Search_Repositories.Models;
 
 namespace Search_Repositories.Controllers
 {
     public class HomeController : Controller
     {
+        private static readonly HttpClient httpClient;
+
+        static HomeController()
+        {
+            httpClient = new HttpClient();
+        }
+
+        [HttpGet]
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult About()
+        [HttpPost]
+        public async Task<ActionResult> GetRepositories(string RepoName)
         {
-            ViewBag.Message = "Your application description page.";
+            string uri = "https://api.github.com/search/repositories?q=" + RepoName;
+            string responseBody = "";
 
-            return View();
-        }
+            try
+            {
+                httpClient.DefaultRequestHeaders.Accept.Clear();
+                httpClient.DefaultRequestHeaders.Add("User-Agent",
+                                                        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.96 Safari/537.36");
+                httpClient.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
+                responseBody = await httpClient.GetStringAsync(uri);
+            }
+            catch (HttpRequestException e)
+            {
 
-            return View();
+                responseBody = e.Message;
+            }
+
+            JObject responseObject = JObject.Parse(responseBody);
+
+            JArray repositories = (JArray)responseObject["items"];
+
+            List<ShowrRpositoriesModel> list = new List<ShowrRpositoriesModel>();
+
+            if (repositories.HasValues)
+            {
+                foreach (JObject repository in repositories)
+                {
+                    ShowrRpositoriesModel tempModel = new ShowrRpositoriesModel
+                    {
+                        RepositoryName = (String)repository.GetValue("name"),
+                        Avatar = (String)repository["owner"]["avatar_url"]
+                    };
+                    list.Add(tempModel);
+                }
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "Search Repositories");
+            }
+
+            return PartialView("_ViewRepositories", list);
         }
     }
 }
---
+++ Search-Repositories/Models/ShowrRpositoriesModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Search_Repositories.Models
+{
+    public class ShowrRpositoriesModel
+    {
+        public String RepositoryName { get; set; }
+        public String Avatar { get; set; }
+    }
+}
